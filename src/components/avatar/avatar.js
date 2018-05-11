974ae8ff58536dd1c5dc5b8947c9a81794354119
ruinan <run7@pitt.edu>
2018-05-10 23:48:38 -0700

    'init'

---
+++ src/components/avatar/avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Avatar extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render (){
+        return (<div>
+            
+        </div>);
+    }
+}
---
+++ src/components/leftbar/leftbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Leftbar (props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Leftbar;
